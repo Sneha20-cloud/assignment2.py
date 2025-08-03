613a2755ce7cc2e9ed3ded35157445ab26360e16
Sneha20-cloud <23ad10sn59@mitsgwl.ac.in>
2025-08-03 23:33:32 +0530

    Add files via upload

---
+++ assi2.py
@@ -0,0 +1,13 @@
+#Task1 solution
+num=int(input("enter a number: "))
+if (num%2==0):
+    print("the number is even")
+else:
+    print("the number is odd")
+
+#Task 2 solution
+sum=0
+for i in range(1,51):
+    sum+=i
+print(f"the sum of numbers from 1 to 50 is: {sum}")
+
